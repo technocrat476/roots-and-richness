f09feb29ee4f6759691de59ba957285efdb8d11c
technocrat476 <rockenter123@gmail.com>
2025-08-17 22:22:25 +0530

    Delete backend directory

--- backend/middleware/notFound.js
+++
@@ -1,7 +0,0 @@
-// middlewares/notFound.js
-export const notFound = (req, res, next) => {
-  res.status(404).json({
-    success: false,
-    message: `Route not found: ${req.originalUrl}`,
-  });
-};
--- backend/test-env.js
+++
@@ -1,6 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-console.log("KEY_ID:", process.env.RAZORPAY_KEY_ID);
-console.log("KEY_SECRET:", process.env.RAZORPAY_KEY_SECRET);
-
